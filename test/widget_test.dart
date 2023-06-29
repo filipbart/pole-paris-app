5fdf89847566a04cf67467ed255de667b50b6e94
Bart <filipbart@wp.pl>
2023-06-29 23:11:32 +0200

    WIP

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,11 +9,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:pole_paris_app/main.dart';
+import 'package:pole_paris_app/services/app_router.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const PoleParisApp());
+    await tester.pumpWidget(PoleParisApp(
+      appRouter: AppRouter(),
+    ));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
